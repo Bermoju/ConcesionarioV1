98f9039271233b341866816257dba9abd568c65a
Bermoju <bermudezmoralesjuan@hotmail.com>
2024-01-29 12:37:19 +0100

    Clase Vehiclulo. Miembros privados, propiedades y constructores

--- ConcesionarioV1/Vehículo.cs
+++ ConcesionarioV1/Vehículo.cs
@@ -6,15 +6,37 @@ using System.Threading.Tasks;
 
 namespace ConcesionarioV1
 {
-    public class Vehículo
+    public class Vehiculo
     {
         // CONSTANTES
-        
+
         // MIENBROS PRIVADOS
+        private string _marca;
+        private string _modelo;
 
         // CONSTRUCTURORES
+        public Vehiculo(string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
 
         // PROPIEDADES
+        public string Marca 
+        {
+            get { return _marca; }
+            set { 
+                    // TODO: Proporcionañr seguridad y protección al mienbro - _marca
+                _marca = value;  } 
+        }
+
+        public string Modelo
+        {
+            get { return _modelo; }
+            set {
+                // TODO: Proporcionañr seguridad y protección al mienbro - _modelo
+                _modelo = value; }
+        }
 
         // MÉTODOS PÚBLICOS
 
